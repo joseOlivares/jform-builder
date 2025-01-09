12c9dc3edc128d0858d2a9e873689930e8edb69e
José Luis Olivares <joseluis_503@yahoo.com>
2025-01-09 16:34:46 -0600

    [VERSION] Se agregan iconos y drag and drop funciona correctamente

--- src/app/pages/form-builder/form-builder.component.ts
+++ src/app/pages/form-builder/form-builder.component.ts
@@ -10,29 +10,28 @@ import {
 } from '@angular/cdk/drag-drop'
 import { FormGroup } from '@angular/forms';
 import { FormlyFieldConfig } from '@ngx-formly/core';
-import { FormlyConfigModule } from '../../core/modules/formly.module';//
+import { FormlyConfigModule } from '../../core/modules/formly.module';//modulo que creamos para la configuración de formly
 
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-form-builder',
-  imports: [CdkDropListGroup, CdkDropList, CdkDrag, FormlyConfigModule, DragDropModule],
+  imports: [CdkDropListGroup, CdkDropList, CdkDrag, FormlyConfigModule, DragDropModule, MatIconModule],
   templateUrl: './form-builder.component.html',
   styleUrl: './form-builder.component.scss'
 })
 export class FormBuilderComponent {
 
 
-  
-
-
-
   form = new FormGroup({});
   model = { name: '', description: '' };
+
+  //Listado de elementos de formulario disponibles
   availableFields: FormlyFieldConfig[] = [
-    { name: 'Text Input', type: 'input', templateOptions: { label: 'Text Input', placeholder: 'Enter text' } },
-    { name: 'Email Input', type: 'input', templateOptions: { label: 'Email Input', placeholder: 'Enter email', type: 'email' } },
-    { name: 'Checkbox', type: 'checkbox', templateOptions: { label: 'Checkbox' } },
-    { name: 'Select', type: 'select', templateOptions: { label: 'Select', options: [{ label: 'Option 1', value: '1' }, { label: 'Option 2', value: '2' }] } },
+    { name: 'Text Input', type: 'input', props: { label: 'Text Input', placeholder: 'Enter text', icon: 'short_text' } },
+    { name: 'Email Input', type: 'input', props: { label: 'Email Input', placeholder: 'Enter email', type: 'email', icon: 'email' } },
+    { name: 'Checkbox', type: 'checkbox', props: { label: 'Checkbox', icon: 'check_box' } },
+    { name: 'Select', type: 'select', props: { label: 'Select', options: [{ value: '1', label: 'Option 1' }, { value: '2', label: 'Option 2' }], icon: 'arrow_drop_down' } },
   ];
 
   selectedFields: FormlyFieldConfig[] = [];
